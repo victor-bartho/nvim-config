4b60bd25222d300f01e68ae281ef2826c41e2459
Victor Bartholomeu <bartholomeuvictor@gmail.com>
2025-12-31 04:14:51 -0300

    add custom config for lualine when catpuccin-mocha colorscheme and make custom config for colorscheme

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,63 +1,36 @@
-return{
+return {
   {
-    "catppuccin/nvim", name = "catppuccin", priority = 1000,
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
     config = function()
-      require("catppuccin").setup{
-        flavour = "mocha",
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        integrations = {
+          treesitter = true,
+          gitsigns = true,
+          lualine = true,
+          neotree = true,
+          telescope = true,
+          which_key = true,
+          cmp = true,
+        },
+        transparent_background = true,
       }
-    vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
-  
-  
-  -- #### ROSE-PINE CONFIG 
-  -- { 
-  --   -- You can easily change to a different colorscheme. Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   -- VICTOR: mudando tema do neovim
-  --   'rose-pine/neovim', priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require('rose-pine').setup {
-  --       variant = 'auto', -- auto, main, moon, or dawn
-  --       dark_variant = 'main', -- main, moon, or dawn
-  --       dim_inactive_windows = false,
-  --       extend_background_behind_borders = true,
-  --       disable_italic = true,
-  --
-  --       enable = {
-  --         terminal = true,
-  --         legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-  --         migrations = true, -- Handle deprecated options automatically
-  --       },
-  --
-  --       highlight_groups = {
-  --         -- Comment = { fg = "foam" },
-  --         -- VertSplit = { fg = "muted", bg = "muted" },
-  --         -- VICTOR: personalizar lineHighlightBackground
-  --         CursorLine = { bg = '#ffffff', blend = 8 },
-  --       },
-  --
-  --       before_highlight = function(group, highlight, palette)
-  --         -- Disable all undercurls
-  --         -- if highlight.undercurl then
-  --         --     highlight.undercurl = false
-  --         -- end
-  --         --
-  --         -- Change palette colour
-  --         -- if highlight.fg == palette.pine then
-  --         --     highlight.fg = palette.foam
-  --         -- end
-  --       end,
-  --     }
-  --     -- FIM DO PASTE DO REQUIRE()
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'rose-pine'
-  --   end,
-  -- },
-
 }
+
+-- ##ROSE-PINE CONFIG
+-- return {
+--   'rose-pine/neovim',
+--   name = 'rose-pine',
+--   priority = 1000,
+--   config = function()
+--     require('rose-pine').setup {}
+--     vim.cmd 'colorscheme rose-pine'
+--     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+--     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+--   end,
+-- }
--- lua/victor/plugins/lualine.lua
+++ lua/victor/plugins/lualine.lua
@@ -1,18 +1,29 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      theme = 'auto',
-      icons_enabled = 'true',
-    },
-    sections = {
-      lualine_a = { { 'mode', right_padding = 2 }, 'branch' },
-      lualine_b = { { 'diff', colored = true }, 'filename' },
-      lualine_c = {}, --{ 'fileformat' }
-      lualine_x = {},
-      lualine_y = { 'filetype', 'progress' },
-      lualine_z = { { 'location', left_padding = 2 } },
-    },
-  },
+  config = function()
+    local colors = require 'lualine.themes.auto' -- pega o tema 'auto'
+
+    -- sobrescrevendo cores dos modos apenas se o colorscheme for catppuccin
+    if vim.g.colors_name == 'catppuccin-mocha' then
+      colors.normal.a.bg = '#cba6f7' -- mauve (roxo) para normal
+      colors.insert.a.bg = '#89b4fa' -- blue (azul) para insert
+      colors.visual.a.bg = '#a6e3a1' -- green (verde) para visual
+    end
+
+    require('lualine').setup {
+      options = {
+        theme = colors,
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = { { 'mode', right_padding = 2 }, 'branch' },
+        lualine_b = { { 'diff', colored = true }, 'filename' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = { { 'location', left_padding = 2 } },
+      },
+    }
+  end,
 }
